7a360ea36547dfee3d1439738bebeefe9ed6e018
farrukhazeem <farrukh90@hotmail.com>
2024-09-19 16:10:23 +0500

    revert getUserByToken

--- src/app/api/getUserByToken/route.ts
+++ src/app/api/getUserByToken/route.ts
@@ -15,35 +15,35 @@ interface JwtPayload {
   
   export async function GET() {
     try {
-      // Access the cookies using `cookies()` 
-      const cookieStore = cookies()
-      const token = cookieStore.get('token')?.value
-        if (!token) {
-        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
-      }
+      // // Access the cookies using `cookies()` 
+      // const cookieStore = cookies()
+      // const token = await cookieStore.get('token')?.value
+      //   if (!token) {
+      //   return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+      // }
   
-      // Verify the JWT token 
-      let decoded: JwtPayload
-      try {
-        decoded = jwt.verify(token, JWT_SECRET) as JwtPayload
-      } catch (error) {
-        return NextResponse.json({ message: 'Invalid token' }, { status: 401 })
-      }
+      // // Verify the JWT token 
+      // let decoded: JwtPayload
+      // try {
+      //   decoded = jwt.verify(token, JWT_SECRET) as JwtPayload
+      // } catch (error) {
+      //   return NextResponse.json({ message: 'Invalid token' }, { status: 401 })
+      // }
   
-      // Fetch the user from the database based on the decoded token
-      const user = await prisma.user.findUnique({
-        where: { id: decoded.id },
-        select: {
-          id: true,
-          name: true,
-        }
-      })
+      // // Fetch the user from the database based on the decoded token
+      // const user = await prisma.user.findUnique({
+      //   where: { id: decoded.id },
+      //   select: {
+      //     id: true,
+      //     name: true,
+      //   }
+      // })
   
-      if (!user) {
-        return NextResponse.json({ message: 'User not found' }, { status: 404 })
-      }
+      // if (!user) {
+      //   return NextResponse.json({ message: 'User not found' }, { status: 404 })
+      // }
   
-      return NextResponse.json(user, { status: 200 })
+      return NextResponse.json("user", { status: 200 })
     } catch (error) {
       console.error(error)
       return NextResponse.json({ message: 'Something went wrong' }, { status: 500 })
